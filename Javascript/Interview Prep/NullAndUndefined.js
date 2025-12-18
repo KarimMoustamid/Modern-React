08b23264592ad1970fb274d7565d356956b2d9e9
KarimMoustamid <moustamid.teck@gmail.com>
2025-12-17 18:54:13 -0600

    Add comments to clarify output and types in NullAndUndefined.js

--- Javascript/Interview Prep/NullAndUndefined.js
+++ Javascript/Interview Prep/NullAndUndefined.js
@@ -1,9 +1,9 @@
 let var1;
-console.log(var1);
-console.log(typeof var1);
+console.log(var1); // undefined
+console.log(typeof var1); // undefined
 
 console.log();
 
 let var2 = null;
-console.log(var2);
-console.log(typeof var2);
+console.log(var2); // null
+console.log(typeof var2); // object
