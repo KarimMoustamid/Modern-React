a526e14b0ecb39b0628b85d7b0b80819396c8afd
KarimMoustamid <moustamid.teck@gmail.com>
2025-12-17 18:39:50 -0600

    Add MapingUsersToUsersNames.js with examples of filtering active users

---
+++ Javascript/Interview Prep/MapingUsersToUsersNames.js
@@ -0,0 +1,32 @@
+
+const users = [
+    { id: 1, name: 'Alice', isActive: true, age: 30 },
+    { id: 2, name: 'Bob', isActive: false, age: 25 },
+    { id: 3, name: 'Cara', isActive: true, age: 28 },
+    { id: 4, name: 'Daniel', isActive: false, age: 35 },
+];
+
+
+const forArray = [];
+// ------------------- First Solution -------------------
+for(let i = 0; i < users.length; i++){
+    if(users[i].isActive){
+        forArray.push(users[i].name);
+    }
+}
+
+console.log("forArray: ", forArray, "");
+console.log();
+
+// ------------------- Second Solution -------------------
+const forEachArray = [];
+users.forEach(user => {
+  if (user.isActive) forEachArray.push(user.name);
+});
+console.log("forEachArray: ", forEachArray, "");
+console.log();
+
+// ------------------- Second Solution -------------------
+
+const mapArray = users.filter(u => u.isActive).map(user => user.name);
+console.log("mapArray: ", mapArray, "");
